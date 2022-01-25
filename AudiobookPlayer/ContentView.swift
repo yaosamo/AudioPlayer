00c9d643893333631b406d891b1e66f01283d712
Yaroslav Samoylov <yaosamo@gmail.com>
2022-01-24 21:56:47 -0800

    progress bar and random colors for playlists

--- AudiobookPlayer/ContentView.swift
+++ AudiobookPlayer/ContentView.swift
@@ -11,7 +11,7 @@ import AVKit
 
 
 extension Text {
-
+    
     func MainFont(_ Size : CGFloat) -> some View {
         
         self.font(.system(size: Size, weight: .medium, design: .rounded))
@@ -20,15 +20,15 @@ extension Text {
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     init() {
-           UITableView.appearance().backgroundColor = .clear
-       }
+        UITableView.appearance().backgroundColor = .clear
+    }
     
     var body: some View {
         VStack {
@@ -40,25 +40,36 @@ struct ContentView: View {
     }
 }
 
+struct ColorModel {
+//    let name: String
+    let colour: UIColor
+}
 
 struct Playlist: View {
     let playlists = ["Marusya", "Brands", "Billionair", "MDS"]
- 
+    var biege = UIColor(red: 0.88, green: 0.83, blue: 0.68, alpha: 1.00)
+    let lightblue = UIColor(red: 0.62, green: 0.78, blue: 0.78, alpha: 1.00)
+    let purple = UIColor(red: 0.40, green: 0.45, blue: 0.94, alpha: 1.00)
+    let brown = UIColor(red: 0.69, green: 0.33, blue: 0.22, alpha: 1.00)
+    var colorslist : [Color] = [Color(red: 0.88, green: 0.83, blue: 0.68), Color(red: 0.62, green: 0.78, blue: 0.78), Color(red: 0.40, green: 0.45, blue: 0.94), Color(red: 0.69, green: 0.33, blue: 0.22)]
+    
     var body: some View {
-
+        
         NavigationView {
             List {
-            ForEach(playlists, id: \.self) { item in
-                NavigationLink(destination: Text("Internal")) {
-                Text(item)
-                        .MainFont(32)
-                        .frame(height: 48)
-                        .navigationBarHidden(true)
-                            }
-                        }
-            .foregroundColor(.white)
-            .listRowBackground(Color.black)
+                ForEach(playlists, id: \.self) { item in
+                    let randomInt = Int.random(in: 0..<4)
+                    NavigationLink(destination: Text("Internal")) {
+                        Text(item)
+                            .MainFont(32)
+                            .frame(height: 48)
+                            .navigationBarHidden(true)
+                            .foregroundColor(colorslist[randomInt])
+                    }
                 }
+                .foregroundColor(.white)
+                .listRowBackground(Color.black)
+            }
             .background(.black)
         }
     }
--- AudiobookPlayer/Player.swift
+++ AudiobookPlayer/Player.swift
@@ -17,14 +17,13 @@ func playSound() {
     guard let path = Bundle.main.path(forResource: "audio", ofType:"m4a") else {
         return }
     let url = URL(fileURLWithPath: path)
-
+    
     do {
         player = try AVAudioPlayer(contentsOf: url)
     } catch let error {
         print(error.localizedDescription)
     }
 }
-
 
 struct Player: View {
     
@@ -33,47 +32,49 @@ struct Player: View {
     let iconstop = "pause.fill"
     @State var playericon = "play.fill"
     
+  
     var body: some View {
         
         VStack {
             HStack {
-            VStack(alignment: .leading){
-                Text("Yaosamo Airpods")
-                    .foregroundColor(Color(red: 0.93, green: 0.59, blue: 0.28))
-                    .MainFont(12)
-                Text("2010.03.10 Mazda")
-                    .MainFont(24)
-                    .foregroundColor(.white)
-                    .padding([.top, .bottom], 1)
-                Text("4:37:22")
-                    .MainFont(12)
-                    .foregroundColor(.white)
-            }
-            .padding(.leading, 32)
+                VStack(alignment: .leading){
+                    Text("Yaosamo Airpods")
+                        .foregroundColor(Color(red: 0.93, green: 0.59, blue: 0.28))
+                        .MainFont(12)
+                    Text("2010.03.10 Mazda")
+                        .MainFont(24)
+                        .foregroundColor(.white)
+                        .padding([.top, .bottom], 1)
+                    Text("4:37:22")
+                        .MainFont(12)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 32)
                 Spacer()
             }
             
             
-            ZStack {
-            Capsule()
-                .fill(Color.white).frame(height: 8)
-                .padding(8)
             
-            Capsule() // progress
-                    .fill(Color.white).frame(width: time, height: 8)
-                .padding(8)
+            ZStack(alignment: .leading) {
+                Capsule()
+                    .fill(Color.white).frame(height: 8)
+                    .padding(8)
+                
+                Capsule() // progress
+                    .fill(Color.red).frame(width: time, height: 8)
+                    .padding(8)
             }
             .padding([.top, .bottom], 40)
             .onAppear(perform: playSound)
             
             HStack {
                 Image(systemName: "backward.fill")
-                        .resizable()
-                        .frame(width: 38, height: 24, alignment: .center)
-                        .foregroundColor(.white)
-        
-            Spacer()
-                               
+                    .resizable()
+                    .frame(width: 38, height: 24, alignment: .center)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
                 Button {
                     let playing = Listening()
                     switch playing {
@@ -84,20 +85,31 @@ struct Player: View {
                         player?.play()
                         playericon = iconstop
                     }
+                    
+                    DispatchQueue.global(qos: .background).async {
+                        
+                        while true {
+                        let screenWidth = UIScreen.main.bounds.width - 24
+                        let currentTime = player?.currentTime
+                        let duration = player?.duration
+                        let labelPosition = CGFloat(currentTime! / duration!) * screenWidth
+                        self.time = labelPosition
+                        }
+                    }
                 }
                 
             label: {
-                    Image(systemName: playericon)
+                Image(systemName: playericon)
                     .font(.system(size: 42.0))
                     .frame(width: 32, height: 44, alignment: .center)
                     .foregroundColor(.white)
-                }
+            }
                 
                 Spacer()
                 Image(systemName: "forward.fill")
-                        .resizable()
-                        .frame(width: 38, height: 24, alignment: .center)
-                        .foregroundColor(.white)
+                    .resizable()
+                    .frame(width: 38, height: 24, alignment: .center)
+                    .foregroundColor(.white)
             }
             .padding([.trailing, .leading], 72)
         }

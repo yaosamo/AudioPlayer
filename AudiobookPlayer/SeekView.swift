040e14c310735bbe7cd54021d4f2427d497f287c
Yaroslav Samoylov <yaosamo@gmail.com>
2022-03-16 15:34:46 -0700

    progress bar autoscrolling fix

--- AudiobookPlayer/SeekView.swift
+++ AudiobookPlayer/SeekView.swift
@@ -24,7 +24,8 @@ struct SeekView: View {
     @State private var seekingTimer : Timer?
     @State private var offset = CGFloat.zero
     @State private var dragInitiated = false
-    @Namespace var endID
+    @Namespace var currentProgress
+    @Namespace var startPoint
     let center = UIScreen.main.bounds.width / 2
     
     var caret: some View {
@@ -32,32 +33,31 @@ struct SeekView: View {
             .fill(playerEngine.status == .empty ? inactiveColor : whiteColor)
             .frame(width: 2, height: 48, alignment: .trailing)
         //compensate 2 for caret
-            .padding([.leading], center-2)
+            .padding([.leading], center-1)
     }
     
     
     var body: some View {
-        
+      
         ScrollViewReader { proxy in
-            
-//            Button("Scroll") {
-//                withAnimation {
-//                    proxy.scrollTo(endID)
-//                }
-//            }
+            Button("Next book") {
+                proxy.scrollTo(startPoint)
+            }
             
             ScrollView(.horizontal) {
+            
                 LazyHStack(alignment: .center, spacing: 0, pinnedViews: [.sectionHeaders], content: {
-                  
                     
+            
                     Section(header: caret) {
-                        
+                    
                         ZStack(alignment: .leading) {
                             
                             // Book's Scroll
                             Rectangle()
                                 .fill(Color(red: 0.17, green: 0.17, blue: 0.18))
                                 .frame(width: playerEngine.bookPlaybackWidth, height: 40)
+                                .id(startPoint)
                             // Caret - playback position
                                 .background(GeometryReader {
                                     Color.clear.preference(key: ViewOffsetKey.self,
@@ -67,33 +67,38 @@ struct SeekView: View {
                                     // Updating offset and applying center to get caret offset
                                     offset = $0+center
                                 }
-                            
+                            // progressing bar
+                            ZStack(alignment: .trailing) {
                             Rectangle()
                                 .fill(whiteColor)
                                 .frame(width: (player?.currentTime ?? 0), height: 8)
-                                .id(endID)
+                            }
+                            .id(currentProgress)
                             
-                        }
+                        } // Zstack
+                        
                         .onAppear {
                             progressUpdate(proxy: proxy)
                         }
+                           
                     }
-                   
+                       
                     
                 })
                 // Trailing padding for whole lazy stack so caret and playback bounces off
                 .padding([.trailing], center)
+
             }
             .frame(height: 96)
             .coordinateSpace(name: "scroll")
             // check if drag happened
-            //            .gesture(DragGesture()
-            //                .onChanged({ _ in
-            //                    dragInitiated = true
-            //                }))
+                        .gesture(DragGesture()
+                            .onChanged({ _ in
+                                dragInitiated = true
+                            }))
             .onChange(of: offset, perform: { newValue in
                 // allow offset to process if drag happened
-                //                if dragInitiated {
+                                if dragInitiated {
                 playerEngine.playerIsSeeking = true
                 // check offset and if it's less than 0 set playback to 0.
                 if offset > 0 && playerEngine.status != .empty {
@@ -106,7 +111,7 @@ struct SeekView: View {
                 // set player to nil and start seeking func
                 seekingTimer = nil
                 SeekPlayerTo(newValue)
-                //                }
+                                }
             })
         }
     }
@@ -115,7 +120,10 @@ struct SeekView: View {
     private func progressUpdate(proxy : ScrollViewProxy) {
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {(_) in
             if playerEngine.status == .playing && !playerEngine.playerIsSeeking  {
-                proxy.scrollTo(endID)
+                withAnimation {
+                proxy.scrollTo(currentProgress)
+                    print("scroll")
+                }
             }
         }
     }

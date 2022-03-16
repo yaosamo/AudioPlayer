cc58a1e9649ad624d5cc636421d1c115473b8278
Yaroslav Samoylov <yaosamo@gmail.com>
2022-03-16 16:03:12 -0700

    new book - reset caret

--- AudiobookPlayer/SeekView.swift
+++ AudiobookPlayer/SeekView.swift
@@ -38,26 +38,27 @@ struct SeekView: View {
     
     
     var body: some View {
-      
+        
         ScrollViewReader { proxy in
-            Button("Next book") {
-                proxy.scrollTo(startPoint)
-            }
-            
+        
             ScrollView(.horizontal) {
-            
+                
                 LazyHStack(alignment: .center, spacing: 0, pinnedViews: [.sectionHeaders], content: {
                     
-            
-                    Section(header: caret) {
                     
+                    Section(header: caret) {
+                        
                         ZStack(alignment: .leading) {
+                            
+                            // starting point
+                            Rectangle()
+                                .frame(width:0)
+                                .id(startPoint)
                             
                             // Book's Scroll
                             Rectangle()
                                 .fill(Color(red: 0.17, green: 0.17, blue: 0.18))
                                 .frame(width: playerEngine.bookPlaybackWidth, height: 40)
-                                .id(startPoint)
                             // Caret - playback position
                                 .background(GeometryReader {
                                     Color.clear.preference(key: ViewOffsetKey.self,
@@ -67,51 +68,49 @@ struct SeekView: View {
                                     // Updating offset and applying center to get caret offset
                                     offset = $0+center
                                 }
+                            
                             // progressing bar
-                            ZStack(alignment: .trailing) {
                             Rectangle()
                                 .fill(whiteColor)
                                 .frame(width: (player?.currentTime ?? 0), height: 8)
-                            }
-                            .id(currentProgress)
-                            
+                                .id(currentProgress)
+             
                         } // Zstack
-                        
                         .onAppear {
                             progressUpdate(proxy: proxy)
                         }
-                           
                     }
-                       
-                    
+                    .onChange(of: playerEngine.bookPlaybackWidth) { newValue in
+                        proxy.scrollTo(startPoint, anchor: .bottomTrailing)
+                    }
                 })
                 // Trailing padding for whole lazy stack so caret and playback bounces off
                 .padding([.trailing], center)
-
+                
             }
             .frame(height: 96)
             .coordinateSpace(name: "scroll")
             // check if drag happened
-                        .gesture(DragGesture()
-                            .onChanged({ _ in
-                                dragInitiated = true
-                            }))
+            .gesture(DragGesture()
+                .onChanged({ _ in
+                    dragInitiated = true
+                }))
             .onChange(of: offset, perform: { newValue in
                 // allow offset to process if drag happened
-                                if dragInitiated {
-                playerEngine.playerIsSeeking = true
-                // check offset and if it's less than 0 set playback to 0.
-                if offset > 0 && playerEngine.status != .empty {
-                    playerEngine.playbackTime = formatTimeFor(seconds: offset)}
-                else { playerEngine.playbackTime = "00:00:00"}
-                
-                // if player exist delete it
-                if seekingTimer != nil {seekingTimer!.invalidate()
+                if dragInitiated {
+                    playerEngine.playerIsSeeking = true
+                    // check offset and if it's less than 0 set playback to 0.
+                    if offset > 0 && playerEngine.status != .empty {
+                        playerEngine.playbackTime = formatTimeFor(seconds: offset)}
+                    else { playerEngine.playbackTime = "00:00:00"}
+                    
+                    // if player exist delete it
+                    if seekingTimer != nil {seekingTimer!.invalidate()
+                    }
+                    // set player to nil and start seeking func
+                    seekingTimer = nil
+                    SeekPlayerTo(newValue)
                 }
-                // set player to nil and start seeking func
-                seekingTimer = nil
-                SeekPlayerTo(newValue)
-                                }
             })
         }
     }
@@ -121,7 +120,7 @@ struct SeekView: View {
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {(_) in
             if playerEngine.status == .playing && !playerEngine.playerIsSeeking  {
                 withAnimation {
-                proxy.scrollTo(currentProgress)
+                    proxy.scrollTo(currentProgress)
                     print("scroll")
                 }
             }

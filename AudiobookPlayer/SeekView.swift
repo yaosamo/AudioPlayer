4465b3844a4e92870570a710279a0c6c7559a22d
Yaroslav Samoylov <yaosamo@gmail.com>
2022-03-18 22:18:13 -0700

    autoscroll, seeking view stable

--- AudiobookPlayer/SeekView.swift
+++ AudiobookPlayer/SeekView.swift
@@ -23,6 +23,7 @@ struct SeekView: View {
     
     @State private var seekingTimer : Timer?
     @State private var offset = CGFloat.zero
+    @State private var scroll = CGFloat.zero
     @State private var dragInitiated = false
     @State private var validView = true
     @Namespace var currentProgress
@@ -34,24 +35,18 @@ struct SeekView: View {
             .fill(playerEngine.status == .empty ? inactiveColor : whiteColor)
             .frame(width: 2, height: 48)
         //compensate 2 for caret
-            .padding([.leading], center-2)
     }
     
     var body: some View {
-        if validView {
+       
         ScrollViewReader { proxy in
             
-            ScrollView(.horizontal) {
-             
-                LazyHStack(alignment: .center, spacing: 0, pinnedViews: [.sectionHeaders], content: {
-                    Section(header: caret) {
+            ScrollView(.horizontal, showsIndicators: false) {
                         ZStack(alignment: .leading) {
-                            
                             // Book's Scroll
                             Rectangle()
                                 .fill(Color(red: 0.17, green: 0.17, blue: 0.18))
                                 .frame(width: playerEngine.bookPlaybackWidth, height: 40)
-
                             // Caret - offset
                                 .background(GeometryReader {
                                     Color.clear.preference(key: ViewOffsetKey.self,
@@ -61,33 +56,22 @@ struct SeekView: View {
                                     // Updating offset and applying center to get caret offset
                                     offset = $0+center
                                 }
-                            
+
+
                             Rectangle()
                                 .fill(.red)
-                                .frame(width: playerEngine.currentProgress ?? 0, height: 16)
+                                .frame(width: playerEngine.currentProgress ?? 0, height: 40)
                                 .id(currentProgress)
-                            
                         }
+                        .padding([.leading, .trailing], center)
                         .onChange(of: playerEngine.currentProgress) { _ in
                             autoScroll(proxy: proxy)
                         }
-                    }
-               
-
-                })
-                .rotationEffect(Angle(degrees: 180))
-                .rotationEffect(Angle(degrees: 180))
-                // Trailing padding for whole lazy stack so caret and playback bounces off
-                .padding([.trailing], center)
-                
             }
-            .frame(height: 40)
-            .padding([.top, .bottom], 28)
+            .frame(height: 56)
+            .padding([.top, .bottom], 12)
             .coordinateSpace(name: "scroll")
-            .onChange(of: playerEngine.bookPlaybackWidth) { _ in
-                validView = false
-                validView = true
-            }
+
             // check if drag happened
             .gesture(DragGesture()
                 .onChanged({ _ in
@@ -96,9 +80,7 @@ struct SeekView: View {
                 }))
             .onChange(of: offset, perform: { newValue in
                 // allow offset to process if drag happened
-                print("off", offset)
                 if dragInitiated {
-                    
                     playerEngine.playerIsSeeking = true
                     // check offset and if it's less than 0 set playback to 0.
                     if offset > 0 && playerEngine.status != .empty {
@@ -113,7 +95,6 @@ struct SeekView: View {
                 }
             })
             
-        }
         }
     }
     

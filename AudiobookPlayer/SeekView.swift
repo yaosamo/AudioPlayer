091ab07059810c39a534dd410f6720b45e78b3df
Yaroslav Samoylov <yaosamo@gmail.com>
2022-03-19 21:31:44 -0700

    scroll and caret fixed

--- AudiobookPlayer/SeekView.swift
+++ AudiobookPlayer/SeekView.swift
@@ -34,67 +34,68 @@ struct SeekView: View {
         Rectangle()
             .fill(playerEngine.status == .empty ? inactiveColor : whiteColor)
             .frame(width: 2, height: 48)
-        //compensate 2 for caret
     }
     
     var body: some View {
-       
+        
         ScrollViewReader { proxy in
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                        ZStack(alignment: .leading) {
-                            // Book's Scroll
-                            Rectangle()
-                                .fill(Color(red: 0.17, green: 0.17, blue: 0.18))
-                                .frame(width: playerEngine.bookPlaybackWidth, height: 40)
-                            // Caret - offset
-                                .background(GeometryReader {
-                                    Color.clear.preference(key: ViewOffsetKey.self,
-                                                           value: -$0.frame(in: .named("scroll")).origin.x)
-                                })
-                                .onPreferenceChange(ViewOffsetKey.self) {
-                                    // Updating offset and applying center to get caret offset
-                                    offset = $0+center
-                                }
-
-
-                            Rectangle()
-                                .fill(.red)
-                                .frame(width: playerEngine.currentProgress ?? 0, height: 40)
-                                .id(currentProgress)
-                        }
-                        .padding([.leading, .trailing], center)
-                        .onChange(of: playerEngine.currentProgress) { _ in
-                            autoScroll(proxy: proxy)
-                        }
-            }
-            .frame(height: 56)
-            .padding([.top, .bottom], 12)
-            .coordinateSpace(name: "scroll")
-
-            // check if drag happened
-            .gesture(DragGesture()
-                .onChanged({ _ in
-                    dragInitiated = true
-                    playerEngine.Stop()
-                }))
-            .onChange(of: offset, perform: { newValue in
-                // allow offset to process if drag happened
-                if dragInitiated {
-                    playerEngine.playerIsSeeking = true
-                    // check offset and if it's less than 0 set playback to 0.
-                    if offset > 0 && playerEngine.status != .empty {
-                        playerEngine.playbackTime = formatTimeFor(seconds: offset)}
-                    else { playerEngine.playbackTime = "00:00:00"}
+            ZStack {
+                ScrollView(.horizontal) {
+                    ZStack(alignment: .leading) {
+                        // Book's Scroll
+                        Rectangle()
+                            .fill(Color(red: 0.17, green: 0.17, blue: 0.18))
+                            .frame(width: playerEngine.bookPlaybackWidth, height: 40)
+                        // Caret - offset
+                            .background(GeometryReader {
+                                Color.clear.preference(key: ViewOffsetKey.self,
+                                                       value: -$0.frame(in: .named("scroll")).origin.x)
+                            })
+                            .onPreferenceChange(ViewOffsetKey.self) {
+                                // Updating offset and applying center to get caret offset
+                                offset = $0+center
+                            }
+                        
+                        Rectangle()
+                            .fill(.red)
+                            .frame(width: playerEngine.currentProgress ?? 0, height: 40)
+                            .id(currentProgress)
+                        
+                    }
+                    .onChange(of: playerEngine.currentProgress) { _ in
+                        autoScroll(proxy: proxy)
+                    }
                     
-                    // if player exist delete it
-                    if seekingTimer != nil { seekingTimer!.invalidate() }
-                    // set player to nil and start seeking func
-                    seekingTimer = nil
-                    SeekPlayerTo(newValue)
+                    .padding([.leading, .trailing], center)
                 }
-            })
-            
+                .frame(height: 56)
+                .padding([.top, .bottom], 12)
+                .coordinateSpace(name: "scroll")
+                
+                // check if drag happened
+                .gesture(DragGesture()
+                    .onChanged({ _ in
+                        dragInitiated = true
+                        playerEngine.Stop()
+                    }))
+                .onChange(of: offset, perform: { newValue in
+                    // allow offset to process if drag happened
+                    if dragInitiated {
+                        playerEngine.playerIsSeeking = true
+                        // check offset and if it's less than 0 set playback to 0.
+                        if offset > 0 && playerEngine.status != .empty {
+                            playerEngine.playbackTime = formatTimeFor(seconds: offset)}
+                        else { playerEngine.playbackTime = "00:00:00"}
+                        
+                        // if player exist delete it
+                        if seekingTimer != nil { seekingTimer!.invalidate() }
+                        // set player to nil and start seeking func
+                        seekingTimer = nil
+                        SeekPlayerTo(newValue)
+                    }
+                })
+                caret
+            }
         }
     }
     
@@ -120,7 +121,6 @@ struct SeekView: View {
             playerEngine.playerIsSeeking = false
             dragInitiated = false
             playerEngine.Play()
-            
         }
     }
 }
